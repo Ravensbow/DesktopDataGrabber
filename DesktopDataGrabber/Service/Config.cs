8bcdb0e48ff581ff6cd3a0e9701fcab3cb2b332c
Jakub Walkowski <walkoskuba@gmail.com>
2020-09-27 17:08:25 +0200

    Dodanie komunikatu o zapisaniu ustawien

--- DesktopDataGrabber/Service/Config.cs
+++ DesktopDataGrabber/Service/Config.cs
@@ -1,9 +1,11 @@
 ﻿using DesktopDataGrabber.Model;
+using DesktopDataGrabber.View;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace DesktopDataGrabber.Service
 {
@@ -14,11 +16,30 @@ namespace DesktopDataGrabber.Service
         public void ChangeSettings(ConfigParams value)
         {
             settings = value;
+            ShowNotificationExecute("Pomyślnie zapisano ustawienia.");
         }
 
         public ConfigParams GetSettings()
         {
             return settings;
+        }
+        private void ShowNotificationExecute(string message)
+        {
+            App.Current.Dispatcher.Invoke(System.Windows.Threading.DispatcherPriority.Normal, new Action(
+                () =>
+                {
+                    foreach (Window window in System.Windows.Application.Current.Windows)
+                    {
+                        string windowName = window.GetType().Name;
+
+                        if (windowName.Equals("NotificationWindow"))
+                        {
+                            return;
+                        }
+                    }
+                    var notify = new NotificationWindow();
+                    notify.Show(message);
+                }));
         }
     }
 }
